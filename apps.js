93898aae02ec2f6e853c3afe574498f93666e759
Ziyad Hammad <ziyad.hammad@outlook.com>
2021-06-22 11:54:46 -0400

    working on html

--- apps.js
+++ apps.js
@@ -1,11 +1,23 @@
 let urlLink = ("https://www.themealdb.com/api/json/v1/1/random.php")
 
+let foodList = document.querySelector(`.food-list`)
+
+
+
 const randomRecipes = async () => {
   try {
     let response = await axios.get(urlLink)
     // console.log(response.data.meals)
-    let mealData = (response.data.meals)
+    let mealData = (response.data.meals[0])
     console.log(mealData)
+    for (let i = 0; i < mealData.length; i++) {
+      let foodName = document.createElement(`h5`)
+      foodName.textContent = mealData.strInstructions
+      foodList.appendChild(foodName)
+
+
+
+    }
     
 
 
