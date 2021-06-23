6d6bbd722f2c6bcbfbd79b2cb9ac05d8b1c3ac77
Ziyad Hammad <ziyad.hammad@outlook.com>
2021-06-23 11:39:46 -0400

    added event listener

--- apps.js
+++ apps.js
@@ -1,41 +1,79 @@
 let urlLink = ("https://www.themealdb.com/api/json/v1/1/random.php")
 
 let foodList = document.querySelector(`.food-list`)
-
+let button = document.querySelector(`button`)
 
 
 const randomRecipes = async () => {
   try {
     let response = await axios.get(urlLink)
-    // console.log(response.data.meals)
-    let mealData = (response.data.meals[0])
-    console.log(mealData)
+    // console.log(response.data)
+    let mealData = response.data.meals
+    // console.log(mealData)
     for (let i = 0; i < mealData.length; i++) {
-
-      let foodName = document.createElement(`h5`)
-      foodName.textContent = mealData.strMeal
-      foodList.appendChild(foodName)
-
-      let foodImage = document.querySelector(`img`)
-      foodImage.setAttribute(`src`, mealData.strMealThumb)
-      foodList.appendChild(foodImage)
-
-      let recipe = document.querySelector(`p`)
-      recipe.textContent = mealData.strInstructions
-      foodList.appendChild(recipe)
-      
+      // console.log(mealData[i].strInstructions)
 
 
+      let food = document.createElement(`div`)
+      // console.log(foodData)
+      foodList.appendChild(food)
 
+      let mealName = document.createElement(`h3`)
+      mealName.textContent = mealData[i].strMeal
+      food.append(mealName)
+
+      let foodPics = document.createElement(`img`)
+      foodPics.setAttribute(`src`, `https://www.themealdb.com/images/media/meals/vtqxtu1511784197.jpg`)
+      food.append(foodPics)
+
+      let foodRecipe = document.createElement(`p`)
+      foodRecipe.textContent = mealData[i].strInstructions
+      food.append(foodRecipe)
 
 
     }
-    
+    return mealData
+
 
 
   } catch (error) {
     console.log(error)
+
   }
 
 }
-randomRecipes()
+
+
+button.addEventListener(`click`, () => {
+  removeFood()
+  randomRecipes()
+})
+
+function removeFood() {
+  while (foodList.lastchild) {
+    foodList.removeChild(foodList.lastChild)
+
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // let randomNumber = Math.floor(Math.random() * mealData.length)
+    // let randomPick = mealData[randomNumber]
+    // console.log(randomPick)
